7682bf3ae5e319d56723594c88bb1b3e80658687
J Miller <stormbeta@gmail.com>
2021-01-01 02:46:20 -0700

    Pack window data into serialized output, convert to render more data and split from color channels

--- main.py
+++ main.py
@@ -1,73 +1,53 @@
 #!/usr/bin/env python3
 
-import png
-from dataclasses import dataclass
-from typing import *
-import numpy as np
-import multiprocessing
-from multiprocessing import Process
 import math
-
-import functools
+import multiprocessing
+import os
 import time
+from collections import namedtuple
+from datetime import datetime
+from multiprocessing import Process
+from struct import pack, unpack, calcsize
+from typing import *
 
-# decorator to measure running time
-def measure_running_time(echo=True):
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapped(*args, **kwargs):
-            t_1 = time.time()
-            ans = func(*args, **kwargs)
-            t_2 = time.time()
-            if echo:
-                print(f'{func.__name__}() running time is {t_2 - t_1:.2f} s')
-            return ans
-        return wrapped
-    return decorator
+import numpy as np
+import png
 
-
-# TODO: Remove this and just use raw array access?
-@dataclass
-class Pixel:
-    r: int
-    g: int
-    b: int
+Resolution: Type[Tuple[int, int]] = namedtuple('Resolution', ['width', 'height'])
+Point: Type[Tuple[float, float]] = namedtuple('Point', ['x', 'y'])
+Window: Type[Tuple[float, float, float, float]] = namedtuple('Window', ['xmin', 'ymin', 'xmax', 'ymax'])
 
 
 class RWindow:
-    xres: int
-    yres: int
-    xmin: float
-    xmax: float
-    ymin: float
-    ymax: float
+    res: Resolution
+    win: Window
+    # xres: int
+    # yres: int
+    # xmin: float
+    # xmax: float
+    # ymin: float
+    # ymax: float
 
     # TODO: This allows non-1-to-1 mapping of aspect ratio between plane and resolution
     #       A) Auto-correct either x or y to match plane
     #       B) Only have plane + resolution
-    def __init__(self, xres: int, yres: int, plane: Tuple[float, float, float, float]):
-        self.xres = xres
-        self.yres = yres
+    # def __init__(self, xres: int, yres: int, plane: Tuple[float, float, float, float]):
+    def __init__(self, resolution: Resolution, window: Window):
+        # TODO: Fix remaining references
+        self.xres = resolution.width
+        self.yres = resolution.height
         self.xmin = plane[0]
         self.ymin = plane[1]
         self.xmax = plane[2]
         self.ymax = plane[3]
+        self.res = resolution
+        self.win = window
 
-        self.dx = ((self.xmax - self.xmin) / self.xres)
-        self.dy = ((self.ymax - self.ymin) / self.yres)
+        self.dx = ((self.win.xmax - self.win.xmin) / self.res.width)
+        self.dy = ((self.win.ymax - self.win.ymin) / self.res.height)
         if self.dx != self.dy:
             print("WARNING: Aspect ratio mismatch!")
-        self.data = np.zeros(shape=(yres, xres*3), dtype=np.uint16)
-
-    def set_pixel(self, x: int, y: int, pixel: Pixel):
-        self.data[y][x*3] = pixel.r
-        self.data[y][x*3+1] = pixel.g
-        self.data[y][x*3+2] = pixel.b
-
-    def get_pixel(self, x: int, y: int) -> Pixel:
-        return Pixel(self.data[y][x*3],
-                     self.data[y][x*3+1],
-                     self.data[y][x*3+2])
+        self.data = np.zeros(shape=(resolution.height, resolution.width*3), dtype=np.uint32)
 
     def xy2plane(self, x: int, y: int) -> Tuple[float, float]:
         return (self.xmin + (x * self.dx),
@@ -84,36 +64,35 @@ class RWindow:
 
     def serialize(self, name: str):
         with open(name, "wb") as fp:
+            fp.write(pack('iidddd', *self.res, *self.win))
             fp.write(self.data.tobytes())
 
-    # WARNING: Will overwrite existing self.data!
-    def deserialize(self, name: str):
+    # TODO: This creates RWindow as read-only
+    @classmethod
+    def deserialize(cls, name: str) -> 'RWindow':
         with open(name, "rb") as fp:
-            load = np.frombuffer(fp.read(), dtype=np.uint16)
-            load.shape = (self.yres, 3*self.xres)
-            self.data = load
-
-    def iterator(self) -> Generator[int, int, Tuple[chr]]:
-        for x in range(self.xres):
-            for y in range(self.yres):
-                yield x, y, self.get_pixel(x, y)
+            resolution = Resolution(*unpack('ii', fp.read(calcsize('ii'))))
+            window = Window(*unpack('dddd', fp.read(calcsize('dddd'))))
+            load = np.frombuffer(fp.read(), dtype=np.uint32)
+            load.shape = (resolution.height, 3*resolution.width)
+        rwin = cls(resolution=resolution, window=window)
+        rwin.data = load
+        return rwin
 
 
 # TODO: Convert RWin to singleton for all values except data blob
-# width = 2048
-# height = 2048
 # max_iter = 1000
 max_iter = 100
-width = 1024
-height = 1024
-plane = (-1.75, -1.25, 0.75, 1.25)
-# plane = (-2.0, -2.0, 2.0, 2.0)
+res = Resolution(1024, 1024)
+# res = Resolution(2048, 2048)
+plane = Window(-1.75, -1.25, 0.75, 1.25)
+# plane = Window(-2.0, -2.0, 2.0, 2.0)
 
 
 def render(dump: str, count: int):
     start_time = time.time()
 
-    rwin = RWindow(width, height, plane)
+    rwin = RWindow(res, plane)
     progress_increment = int(count/100)
 
     # Define the plane we wish to render in 4D space
@@ -159,31 +138,49 @@ def render(dump: str, count: int):
             limit += 1
             xpoints[i] = z.real
             ypoints[i] = z.imag
-            # points.append((z.real, z.imag))
-        if not escapes:
+        for i in range(limit):
             # Weirdly, using numpy here is actually slower than calling rwin.plane2xy
             # Guessing it's due to the relatively small size of the points lists
             # npx = (xpoints - rwin.xmin) / rwin.dx - 1
             # npy = rwin.yres - ((ypoints - rwin.ymin)/rwin.dy) - 1
-            for i in range(limit):
-                x, y = rwin.plane2xy(xpoints[i], ypoints[i])
-                # x = int(npx[i])
-                # y = int(npy[i])
-                if x < 0 or x >= rwin.xres or y < 0 or y >= rwin.yres:
-                    continue
-                # TODO: This is kind of silly - we should decouple color channels from render data
-                #       That way render data channels can focus on information only available at render time
-                #       All colorization calculations are already deferred to post-merge anyways
-                rwin.data[y, x*3] += 1
-                rwin.data[y, x*3 + 1] += 1
-                rwin.data[y, x*3 + 2] += 1
+            # x = int(npx[i])
+            # y = int(npy[i])
+            x, y = rwin.plane2xy(xpoints[i], ypoints[i])
+            # Ignore any points outside the render space
+            if x < 0 or x >= rwin.xres or y < 0 or y >= rwin.yres:
+                continue
+            if escapes:
+                rwin.data[y, x*3] += 1      # Baseline incrementor
+                rwin.data[y, x*3 + 1] += i  # Based on iteration count to reach this point
+            # TODO: Leave disabled for now, adds _WAY_ too much render time to do both inner/outer traces
+            #       unless actually intending to use both, even for relatively small trace/iteration counts
+            # else:
+            #     for i in range(limit):
+            #         rwin.data[y, x * 3 + 2] += 1  # Non-escaping incrementor
     if (dump == 'render0.dat'):
         print(f'{dump} running time is {start_time - time.time():.2f} s')
     rwin.serialize(dump)
 
 
+def np_log_curve(arr0, arr1, inset, outset, maximum: int):
+    k = 255/math.log2(maximum)
+    arr1[:, outset::3] = np.multiply(k, np.log2(arr0[:, inset::3]))
+
+def np_sqrt_curve(arr0, arr1, inset, outset, maximum: int):
+    k = 255/math.sqrt(maximum)
+    arr1[:, outset::3] = k * np.sqrt(arr0[:, inset::3])
+
+def np_quasi_curve(arr0, arr1, inset, outset, maximum: int):
+    linear_k = 255/maximum
+    sqrt_k = 255/math.sqrt(maximum)
+    arr1[:, outset::3] = (sqrt_k*np.sqrt(arr0[:, inset::3]) + linear_k*arr0[:, inset::3]) / 2
+
+def np_linear(arr0, arr1, inset, outset, maximum: int):
+    arr1[:, outset::3] = (255/maximum) * arr0[:, inset::3]
+
+
 if __name__ == '__main__':
-    workers = multiprocessing.cpu_count()
+    workers = multiprocessing.cpu_count() - 1
 
     # skip_render = True
     skip_render = False
@@ -200,94 +197,50 @@ if __name__ == '__main__':
             proc.join()
 
         # MERGE
-        rwin = RWindow(width, height, plane)
+        rwin = RWindow(resolution=res, window=plane)
+        print(f"Merging {workers} datasets...")
         for i in range(workers):
-            print(f"Merging {i+1}/{workers}")
-            pdat = RWindow(width, height, plane)
-            pdat.deserialize(f"render{i}.dat")
+            pdat = RWindow.deserialize(f"render{i}.dat")
             rwin.data += pdat.data
         rwin.serialize("render.dat")
-        # TODO: Delete unmerged data
+        [os.remove(f"render{i}.dat") for i in range(workers)]
+        output = RWindow(resolution=res, window=plane)
     else:
         # Allow skipping render to play around with coloring/gradients without re-rendering every single time
-        # NOTE: if you change plane/resolution, you must re-render!
-        # TODO: Why do I get read-only here without double init?
-        rwin0 = RWindow(width, height, plane)
-        rwin0.deserialize("render.dat")
-        rwin = RWindow(width, height, plane)
-        rwin.data += rwin0.data
+        # NOTE: Safe to use on render data from different plane/resolution, as that's stored alongside point data
+        rwin = RWindow.deserialize(f"render.dat")
+        output = RWindow(resolution=rwin.res, window=rwin.win)
 
 
-    print("Normalizing...")
+
+    print("Colorizing...")
+
+    # TODO: Technically this no longer makes sense as all render "color" channels are the same
+    #       Though if I make the channels represent different kinds of render data we'll still need these - might make more sense as array
     rmax, gmax, bmax = 1, 1, 1
 
-    # Native numpy is ludicrously faster than iterating in python
-    rmax = np.max(rwin.data[:, 0::3])
-    gmax = np.max(rwin.data[:, 1::3])
-    bmax = np.max(rwin.data[:, 2::3])
-
-    def np_log_curve(arr, offset, maximum: int):
-        k = 255/math.log2(maximum)
-        arr[:, offset::3] = np.multiply(k, np.log2(arr[:, offset::3]))
-
-    def np_sqrt_curve(arr, offset, maximum: int):
-        k = 255/math.sqrt(maximum)
-        arr[:, offset::3] = k * np.sqrt(arr[:, offset::3])
+    # Native numpy is ludicrously faster than iterating manually in python
+    nmax = np.max(rwin.data[:, 0::3])
+    itermax = np.max(rwin.data[:, 1::3])
+    innermax = np.max(rwin.data[:, 2::3])
+    print(f"nmax: {rmax}")
+    print(f"itermax: {gmax}")
+    print(f"innermax: {bmax}")
 
     #         red = (linear_curve(red, rmax) + sqrt_curve(red, rmax)) / 2
-    def np_quasi_curve(arr, offset, maximum: int):
-        linear_k = 255/maximum
-        sqrt_k = 255/math.sqrt(maximum)
-        arr[:, offset::3] = (sqrt_k*np.sqrt(arr[:, offset::3]) + linear_k*arr[:, offset::3]) / 2
-
-    def np_linear(arr, offset, maximum: int):
-        arr[:, offset::3] = (255/maximum) * arr[:, offset::3]
-
-
     # rwin.data[:, 0::3] = (255/rmax) * rwin.data[:, 0::3]
-    np_sqrt_curve(rwin.data, 0, rmax/3)
+    np_sqrt_curve(rwin.data, output.data, 0, 0, nmax/3)
     # np_log_curve(rwin.data, 0, rmax*3)
-    np_sqrt_curve(rwin.data, 1, gmax/5)
-    np_log_curve(rwin.data, 2, bmax*1.5)
+    np_sqrt_curve(rwin.data, output.data, 0, 1, nmax/5)
+    np_log_curve(rwin.data, output.data, 0, 2, nmax*1.5)
 
     # np_linear(rwin.data, 0, rmax/7)
     # np_linear(rwin.data, 1, gmax/7)
     # np_log_curve(rwin.data, 2, bmax*3)
-    rwin.data = np.minimum(rwin.data, 255)
 
-    floor = 0
-    def log_curve(value: int, maximum: int):
-        value = max(value - floor, 0)
-        k = 255/math.log2(maximum)
-        return k*math.log2(value+1)
-
-    def sqrt_curve(value: int, maximum: int):
-        return (255/math.sqrt(maximum)) * math.sqrt(value)
-
-    def linear_curve(value: int, maximum: int):
-        return value*(255/maximum)
-
-    print(f"red.max: {rmax}")
-    print(f"green.max: {gmax}")
-    print(f"blue.max: {bmax}")
-
-    # TODO: This is absurdly slow for some reason
-    # Color normalization - different trace counts / resolution will produce different maximum scales
-    if False:
-        for x in range(rwin.xres):
-            for y in range(rwin.yres):
-                red = rwin.data[y][x * 3]
-                green = rwin.data[y][x * 3 + 1]
-                blue = rwin.data[y][x * 3 + 2]
-                blue = log_curve(blue, rmax)
-                green = sqrt_curve(green, gmax)
-                # green = (linear_curve(green, rmax) + sqrt_curve(green, rmax)) / 2
-                # red = (linear_curve(red, rmax) + sqrt_curve(red, rmax)) / 2
-                # red = sqrt_curve()
-                red = linear_curve(red, rmax)
-                rwin.data[y][x * 3] = red
-                rwin.data[y][x * 3 + 1] = green
-                rwin.data[y][x * 3 + 2] = blue
+    # Clamp values
+    output.data = np.minimum(output.data, 255)
 
     print("Saving...")
-    rwin.save("budda.png")
+    # TODO: Include resolution/iteration count in filename?
+    output.save(f"renders/nebula_{int(datetime.now().timestamp())}.png")
